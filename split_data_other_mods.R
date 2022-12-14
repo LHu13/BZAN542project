6d06a1c8d2b9334e37f6f0a12a88e66696e7ba88
Jackson DeBord <jdebord4@vols.utk.edu>
2022-12-14 16:24:18 -0500

    Updated to divide training data into smaller amounts for easier storage

--- split_data_other_mods.R
+++ split_data_other_mods.R
@@ -40,11 +40,25 @@ csgo.test = csgo_processed[-tr.set,]
 table(csgo.train$T_win); table(csgo.test$T_win)
 
 
+#file limit still exceeds 100 MB, split train data into two dfs
+
+#take ceiling of length / 2
+spl.n = ceiling(nrow(csgo.train)/2)
+
+#train 1
+csgo.train1 = csgo.train[1:spl.n,]
+
+#train 2
+csgo.train2 = csgo.train[-(1:spl.n),]
+
 
 ####saving compressed files for each
 
-#training data
-fwrite(csgo.train, "D:/csgo_train.gz")
+#training data 1
+fwrite(csgo.train1, "D:/csgo_train1.gz")
+
+#training data 2
+fwrite(csgo.train2, "D:/csgo_train2.gz")
 
 #test data
 fwrite(csgo.test, "D:/csgo_test.gz")
