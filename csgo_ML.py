74ff8b3596e4c64b9acec5bc55fb85e7961612dd
Liana Hu <lianali1202@hotmail.com>
2022-12-14 08:51:53 -0500

    Added some checkpoint messages and clarification comments.

--- csgo_ML.py
+++ csgo_ML.py
@@ -15,9 +15,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 #change to whereever your csv is stored.
-dataframe = pd.DataFrame(pd.read_csv("C:\Users\liana\OneDrive\Desktop\all_game_state_data_train.csv"))
-
-
+print('Started loading data')
+dataframe = pd.DataFrame(pd.read_csv("C:\\Users\\liana\\OneDrive\\Desktop\\all_game_state_data_train.csv"))
+print('Finished loading data')
 #%%
 ## DATA PRE-PROCESSING ##
 
@@ -35,7 +35,7 @@ dataframe_subset = dataframe[['seconds', 'is_bomb_planted', 'bomb_site', 'CT1_he
                               'CT4_health', 'CT5_health', 'T1_health', 'T2_health', 'T3_health', 'T4_health', 'T5_health', 'map',
                               'round_type', 'ct_eq_val', 't_eq_val', 'T1_dead', 'T2_dead', 'T3_dead', 'T4_dead', 'T5_dead',
                               'CT1_dead', 'CT2_dead', 'CT3_dead', 'CT4_dead', 'CT5_dead', 'T_dead', 'CT_dead']]
-
+print('Finished subsetting data')
 #Change the the bomb_site, map, and round_type variables as integers.
 dataframe_subset[['bomb_site', 'map', 'round_type']] = dataframe_subset[['bomb_site', 'map', 'round_type']].apply(LabelEncoder().fit_transform)
 csgo_data = dataframe_subset.values #takes off the headers so it is just values
@@ -51,12 +51,13 @@ encoder.fit(Y)
 Y = encoder.transform(Y)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25) #splits the data into training and testing data subsets
-
+print('Finished splitting data')
 
 #%%
 ## MODEL CREATION AND TRAINING###
 
 # Creates the actual model
+print('Started training')
 model = tf.keras.models.Sequential([ #chooses model layout
     tf.keras.layers.Normalization(axis=None), #normalize the data and convert to z-score
     tf.keras.layers.Dense(29, input_shape=(29,), activation='relu'), #29 nodes for input
@@ -77,7 +78,7 @@ model.evaluate(X_test, Y_test, verbose=2) #conducts the model on the test subset
 #%%
 y_pred = model.predict(X_test) #conducts the model on the test subset and finds number of prediction later
 #%%
-model.save('first_training_50_epoch') #saves model so we do not have to retrain
+model.save('C:\\Users\\liana\\OneDrive\\Desktop\\CSGO_ML_first_training_50_epoch') #saves model so we do not have to retrain
 #%%
 matrix = sklearn.metrics.confusion_matrix(Y_test, np.rint(y_pred).astype(int)) #creates confusion matrix
 #%%
