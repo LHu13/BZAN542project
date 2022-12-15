28091f8a91c24d13f648639962284aeb0fef472d
LHu13 <112121959+LHu13@users.noreply.github.com>
2022-12-14 21:33:47 -0500

    Delete read_gzip_data.py

--- read_gzip_data.py
+++
@@ -1,9 +0,0 @@
-##import pandas/numpy
-import numpy as np
-import pandas as pd
-
-##decompress and read data
-#change pathname to match data location
-csgo_raw = pd.read_csv("C:/Users/Jackson DeBord/Documents/Fall_22/Data_Mining/BZAN542project/raw_csgo_data.gzip",
-                        compression = 'gzip',
-                        parse_dates = [2])
